e203f445ff18f827796aff9778d1ce710cc0c213
Aleksey Timin <atimin@gmail.com>
2021-10-21 21:26:46 +0200

    implement save request

--- app/config.py
+++ app/config.py
@@ -1,11 +1,12 @@
 """Configuration"""
-from pydantic import BaseSettings, Field, HttpUrl
+from pydantic import BaseSettings, Field, HttpUrl, AnyHttpUrl
 
 
 class Config(BaseSettings):
     """App configuration"""
     gutendex_url: HttpUrl = Field("https://gutendex.com")
     gutenberg_url: HttpUrl = Field("https://gutenberg.org")
+    text_storage_url: AnyHttpUrl = Field('http://nginx:8080')
     authjwt_secret_key: str = Field(description='Secret key for JWT',
                                     env='secret_key', default='secret')
 
--- app/main.py
+++ app/main.py
@@ -1,9 +1,8 @@
 """Web application"""
 import logging
-from typing import Optional, Dict
-from urllib.parse import urlparse as url
+from typing import List
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, Header
 from fastapi_jwt_auth import AuthJWT
 from fastapi_jwt_auth.exceptions import AuthJWTException
 from starlette.requests import Request
@@ -11,14 +10,12 @@ from starlette.responses import JSONResponse
 import aiohttp
 
 from app.config import CONFIG
-from app.schemas import Book
+from app.model import Book, LANG_CODE_MAP
 
 logging.basicConfig(level='DEBUG')
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
-
-SUPPORTED_FORMATS = ["text/plain", "text/plain;charset=utf-8"]
 
 
 @AuthJWT.load_config
@@ -42,18 +39,6 @@ def authjwt_exception_handler(_request: Request, exc: AuthJWTException):
     )
 
 
-def _find_supported_format(formats: Dict[str, str]) -> Optional[str]:
-    for mime_type, link in formats.items():
-        if mime_type.strip().replace(' ', '').lower() in SUPPORTED_FORMATS:
-            return link
-
-    return None
-
-
-def _substitute_domain(link):
-    return CONFIG.gutenberg_url + url(link).path
-
-
 @app.get('/gutenberg/search')
 async def search(q: str, authorize: AuthJWT = Depends()):
     """Search texts in Gutendex
@@ -66,22 +51,41 @@ async def search(q: str, authorize: AuthJWT = Depends()):
             data = await resp.json()
             logger.info(data)
 
-            books = []
-            for book in data['results']:
-                formats_ = book['formats']
-                download_link = _find_supported_format(formats_)
+            books: List[Book] = []
+            for result in data['results']:
+                book = Book.from_gutendex(result)
+                if book:
+                    books.append(book)
 
-                if download_link:
-                    download_link = _substitute_domain(download_link)
-                    cover_link = None
-                    if 'image/jpeg' in formats_:
-                        cover_link = _substitute_domain(formats_['image/jpeg'])
+            return books
 
-                    author = book['authors'][0]['name'] if len(book['authors']) > 0 else 'unknown'
-                    books.append(Book(id=book['id'], title=book['title'],
-                                      language=book['languages'][0],
-                                      author=author,
-                                      bookUrl=download_link,
-                                      coverUrl=cover_link))
 
-    return books
+@app.post('/gutenberg/save/{book_id}')
+async def save(book_id: str, lang: str, authorize: AuthJWT = Depends(), authorization: str = Header(None)):
+    # authorize.jwt_required()
+
+    search_url = f'{CONFIG.gutendex_url}/books?ids={book_id}'
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(search_url) as resp:
+            data = await resp.json()
+            book = Book.from_gutendex(data['results'][0])
+
+        async with session.get(book.book_url) as resp:
+            content = await resp.text()
+
+        new_text = {
+            'title': book.title,
+            'author': book.author,
+            'sourceLang': LANG_CODE_MAP[book.language],
+            'targetLang': lang,
+            'content': content
+        }
+
+        async with session.post(CONFIG.text_storage_url + '/text/create', json=new_text,
+                                headers={'Authorization': authorization}) as resp:
+            data = await resp.json()
+            if not resp.ok:
+                raise HTTPException(status_code=resp.status, detail=data)
+
+            return data

2d3188e0b7058ca188c77b3f06751f45ac013a4d
Aleksey Timin <atimin@gmail.com>
2021-10-22 00:39:24 +0200

    change nginx url

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ class Config(BaseSettings):
     """App configuration"""
     gutendex_url: HttpUrl = Field("https://gutendex.com")
     gutenberg_url: HttpUrl = Field("https://gutenberg.org")
-    text_storage_url: AnyHttpUrl = Field('http://nginx:8080')
+    text_storage_url: AnyHttpUrl = Field('https://nginx')
     authjwt_secret_key: str = Field(description='Secret key for JWT',
                                     env='secret_key', default='secret')
 

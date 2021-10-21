96a6a5128d60d3673285e34c6209a176a4f3da4d
Aleksey Timin <atimin@gmail.com>
2021-10-22 00:54:48 +0200

    use http

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ class Config(BaseSettings):
     """App configuration"""
     gutendex_url: HttpUrl = Field("https://gutendex.com")
     gutenberg_url: HttpUrl = Field("https://gutenberg.org")
-    text_storage_url: AnyHttpUrl = Field('https://text-storage')
+    text_storage_url: AnyHttpUrl = Field('http://text-storage')
     authjwt_secret_key: str = Field(description='Secret key for JWT',
                                     env='secret_key', default='secret')
 
